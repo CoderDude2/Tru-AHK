7d744cdadd4d1444368b4b650db857668a10e8b5
CoderDude2 <isaacboots1@gmail.com>
2024-10-29 17:04:50 -0700

    Added a build script in python

---
+++ build.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import shutil
+
+files = [
+    "master_switch.ahk",
+    "text_x.ahk"
+]
+
+def build():
+    if(not os.path.exists("./build")):
+        os.mkdir("./build")
+    
+    for file in files:
+        f_name = file.split(".")[0]
+        os.system(fr'C:\Users\TruUser\AppData\Local\Programs\AutoHotkey\Compiler\Ahk2Exe.exe /in .\{file} /out ./build/{f_name}.exe')
+
+def release():
+    if(not os.path.exists("./dist")):
+        os.mkdir("./dist")
+    else:
+        shutil.rmtree("./dist")
+        os.mkdir("./dist")
+    build()
+    for file in os.listdir("./build"):
+        shutil.copy(
+            os.path.join("./build", file), 
+            os.path.join("./dist", file))
+    shutil.copytree("./resources/", "./dist/resources")
+    shutil.copy("./config.ini", "./dist/config.ini", )
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        match sys.argv[1]:
+            case 'build':
+                build()
+            case 'release':
+                release()
+            case _:
+                print("Invalid Command!")
